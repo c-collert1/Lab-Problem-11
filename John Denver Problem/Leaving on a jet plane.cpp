7b2177771da64dceffa40ddd9dd3ab4508b3e4f7
Caleb Collert <c-collert.1@onu.edu>
2024-04-25 08:25:49 -0400

    Airplane display completed

--- John Denver Problem/Leaving on a jet plane.cpp
+++ John Denver Problem/Leaving on a jet plane.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
 
+const int ROWS = 13;
+const int COLUMNS = 8;
+int thisRow;
+
+void displaySeatColumn(char seats[][ROWS], int column);
+
 int main() {
+	bool gettingInput = true;
+	bool availableSeat = true;
+	int columnCheck = 0;
+	int rowCheck = 404;
+	int seatCheck = 404;
+	//Seating chart initialized
+	char seatChart[COLUMNS][ROWS] = { {'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', ' '},
+									  {'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', ' '},
+									  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+									  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '1', '1', '1', '1'},
+									  {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3'},
+									  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+									  {'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'},
+									  {'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'} };
 
+	for (int i = 1; i < 9; i++) {
+		thisRow = i;
+		displaySeatColumn(seatChart, thisRow);
+	}
+}
 
+void displaySeatColumn(char seats[][ROWS], int column) {
+	for (int i = 0; i < ROWS; i++) {
+		cout << seats[column - 1][i] << " ";
+	}
+	cout << endl;
 }
