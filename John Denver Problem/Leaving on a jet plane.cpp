1dc140f1d3495b95cf59c709d284252c05a8751c
Caleb Collert <c-collert.1@onu.edu>
2024-04-25 08:27:31 -0400

    finding exact plane seat completed

--- John Denver Problem/Leaving on a jet plane.cpp
+++ John Denver Problem/Leaving on a jet plane.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 const int ROWS = 13;
 const int COLUMNS = 8;
-int thisRow;
+int thisRow = -1;
+char thisSeat = ' ';
 
+int findRowIndex(int thisRow);
+int findSeatIndex(char thisSeat);
 void displaySeatColumn(char seats[][ROWS], int column);
 
 int main() {
@@ -27,6 +30,29 @@ int main() {
 		thisRow = i;
 		displaySeatColumn(seatChart, thisRow);
 	}
+
+
+	do {
+		cout << endl << "What seat would you like? (ex. 12A) Press Q to Quit: ";
+		cin >> thisRow >> thisSeat;
+		system("cls");
+		if (!cin.fail()) {
+			if (findRowIndex(thisRow) != 404) {
+				columnCheck = findRowIndex(thisRow);
+			}
+			if (findSeatIndex(thisSeat) != 404) {
+				if (findRowIndex(thisRow) == 404) {
+					rowCheck = 404;
+				}
+				else {
+					rowCheck = findSeatIndex(thisSeat);
+				}
+			}
+		}
+		cout << "Row: " << rowCheck << endl;
+		cout << "Column: " << columnCheck << endl;
+
+	} while (!cin.fail());
 }
 
 void displaySeatColumn(char seats[][ROWS], int column) {
@@ -34,4 +60,38 @@ void displaySeatColumn(char seats[][ROWS], int column) {
 		cout << seats[column - 1][i] << " ";
 	}
 	cout << endl;
+}
+
+int findRowIndex(int thisRow) {
+	int seatSelected = findSeatIndex(thisSeat);
+	if (seatSelected == 0 || seatSelected == 1) {
+		if (thisRow > -1 && thisRow < 13) {
+			return (thisRow - 1);
+		}
+	}
+
+	if (seatSelected == 6 || seatSelected == 7) {
+		if (thisRow > -1 && thisRow < 14) {
+			return (thisRow - 1);
+		}
+	}
+	return 404;
+}
+int findSeatIndex(char thisSeat) {
+
+	if (toupper(thisSeat) == 'D') {
+		return 0;
+	}
+	else if (toupper(thisSeat) == 'C') {
+		return 1;
+	}
+	else if (toupper(thisSeat) == 'B') {
+		return 6;
+	}
+	else if (toupper(thisSeat) == 'A') {
+		return 7;
+	}
+	else {
+		return 404;
+	}
 }
